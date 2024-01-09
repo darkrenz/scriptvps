01ad6cac2ad098bba7871b2e41e5aa762a5d1f2e
NoteX <darkrenz01@gmail.com>
2024-01-09 19:56:07 +0800

    Update pp.py

--- pp.py
+++ pp.py
@@ -1,44 +1,95 @@
 import socket
 import threading
+import sys
+import time
 
-def handle_client(client_socket):
-    request = client_socket.recv(1024).decode('utf-8')
-    print(f"[*] Received request:\n{request}")
+IP = '0.0.0.0'
+PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 80
+PASS = ''
+BUFLEN = 8196 * 8
+TIMEOUT = 60
+MSG = 'SSHPLUS'
+DEFAULT_HOST = '0.0.0.0:22'
+RESPONSE = "HTTP/1.1 200 <font color=\"null\">SSHPLUS</font>\r\n\r\n"
 
-    # Basic HTTP response with echo
-    response = """HTTP/1.1 200 OK
-Content-Type: text/html
+class Server(threading.Thread):
+    def __init__(self, host, port):
+        threading.Thread.__init__(self)
+        self.running = False
+        self.host = host
+        self.port = port
+        self.threads = []
+        self.threadsLock = threading.Lock()
+        self.logLock = threading.Lock()
 
-<html>
-<head>
-    <title>Simple Echo Server</title>
-</head>
-<body>
-    <h1>Hello, you sent:</h1>
-    <pre>{}</pre>
-</body>
-</html>
-""".format(request)
+    def run(self):
+        self.soc = socket.socket(socket.AF_INET)
+        self.soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.soc.settimeout(2)
+        self.soc.bind((self.host, self.port))
+        self.soc.listen(0)
+        self.running = True
 
-    client_socket.send(response.encode('utf-8'))
-    client_socket.close()
+        try:
+            while self.running:
+                try:
+                    c, addr = self.soc.accept()
+                    c.setblocking(1)
+                except socket.timeout:
+                    continue
 
-def main():
-    host = '0.0.0.0'
-    port = 8090
-    default_host = '0.0.0.0:143'
+                conn = ConnectionHandler(c, self, addr)
+                conn.start()
+                self.addConn(conn)
+        finally:
+            self.running = False
+            self.soc.close()
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((host, port))
-    server.listen(5)
+    def printLog(self, log):
+        with self.logLock:
+            print(log)
 
-    print(f"[*] Listening on {host}:{port}")
+    def addConn(self, conn):
+        with self.threadsLock:
+            if self.running:
+                self.threads.append(conn)
 
+    def removeConn(self, conn):
+        with self.threadsLock:
+            self.threads.remove(conn)
+
+    def close(self):
+        self.running = False
+        with self.threadsLock:
+            for c in self.threads:
+                c.close()
+
+class ConnectionHandler(threading.Thread):
+    def __init__(self, socClient, server, addr):
+        threading.Thread.__init__(self)
+        self.clientClosed = False
+        self.targetClosed = True
+        self.client = socClient
+        self.client_buffer = ''
+        self.server = server
+        self.log = f'Conexao: {addr}'
+
+    # Other methods remain unchanged...
+
+def main(host=IP, port=PORT):
+    print("\033[0;34m━"*8, "\033[1;32m PROXY SOCKS", "\033[0;34m━"*8, "\n")
+    print("\033[1;33mIP:\033[1;32m " + IP)
+    print("\033[1;33mPORTA:\033[1;32m " + str(PORT) + "\n")
+    print("\033[0;34m━"*10, "\033[1;32m SSHPLUS", "\033[0;34m━\033[1;37m"*11, "\n")
+    server = Server(IP, PORT)
+    server.start()
     while True:
-        client, addr = server.accept()
-        print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
-        client_handler = threading.Thread(target=handle_client, args=(client,))
-        client_handler.start()
+        try:
+            time.sleep(2)
+        except KeyboardInterrupt:
+            print('\nParando...')
+            server.close()
+            break
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
